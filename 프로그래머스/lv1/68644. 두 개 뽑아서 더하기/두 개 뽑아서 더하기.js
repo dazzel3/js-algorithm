0603c3c50541e899fd4ad637c6f7337dc1b7f5c1
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2022-09-17 02:19:25 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.85 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+const getCombi = (arr, num) => {
+    const results = []
+    if (num === 1) return arr.map((v) => [v])
+    
+    arr.forEach((fixed, idx, origin) => {
+        const rest = origin.slice(idx+1)
+        const combi = getCombi(rest, num-1)
+        const attached = combi.map((v) => +fixed + +v)
+        results.push(...attached)
+    })
+    return results.filter((v, idx) => results.indexOf(v) === idx).sort((a,b) => a-b)
+}
+
+function solution(numbers) {
+    return getCombi(numbers, 2)
+}

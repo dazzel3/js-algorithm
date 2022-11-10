93a47d2124ae81398c2c7e7c6a2b30bbb70ccc7d
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2022-11-11 00:11:19 +0900

    [unrated] Title: 과일 장수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,20 @@
+function solution(k, m, score) {
+    score.sort((a,b) => b-a)
+    const rest = score.length % m
+    
+    if (rest) score.splice(-rest)
+    
+    const box = []
+    let count = 0, answer = 0
+    
+    for (apple of score) {
+        count += 1
+        
+        if (count === m) {
+            answer += apple * m
+            count = 0
+        }
+    }
+    return answer
+    
+}

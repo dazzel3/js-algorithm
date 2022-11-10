bd0eb35e01ae5c37cf256f5a7b51e4d86f6bc84c
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2022-11-10 23:48:37 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 5.33 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,26 @@
+function solution(bridge_length, weight, truck_weights) {
+    let bridge_weights = 0
+    let timer = 0
+    
+    const bridge = Array(bridge_length).fill(0)
+    
+    timer++
+    bridge.shift()
+    bridge_weights += truck_weights[0]
+    bridge.push(truck_weights.shift())
+    
+    console.log(bridge, truck_weights)
+    
+    while (bridge_weights > 0) {
+        timer++
+        bridge_weights -= bridge.shift()
+        
+        if (truck_weights && bridge_weights + truck_weights[0] <= weight) {
+            bridge_weights += truck_weights[0]
+            bridge.push(truck_weights.shift())
+        } else {
+            bridge.push(0)
+        }
+    }
+    return timer
+}

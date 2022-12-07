6ee6a49c56a2f9f8ec965b1143c41efaa36e60d0
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2022-12-08 08:04:18 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,12 +1,9 @@
 function solution(food) {
     let result = ''
-    food = food.slice(1).map(v => Math.floor(v/2))
     
-    for (let i=0; i<food.length; i++) {
-        for (let j=0; j<food[i]; j++) {
-            result += i+1
-        }
+    for (let i=1; i<food.length; i++) {
+        result += String(i).repeat(Math.floor(food[i]/2))
     }
     
-    return result + '0' + result.split('').reverse().join('')
+    return `${result}0${result.split('').reverse().join('')}`
 }

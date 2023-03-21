ef2de7ff9e6b59154caa9932244009533f1ceb49
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2023-03-22 03:15:12 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,6 +1,7 @@
 function solution(d, budget) {
     let arr = d.sort((a,b) => a-b)
     let result = 0
+    
     for (let i=0; i<arr.length; i++) {
         budget -= arr[i]
         if (budget < 0) break

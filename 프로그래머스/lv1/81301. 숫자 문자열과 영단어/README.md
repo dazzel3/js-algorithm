36da789837e05300324f9327997c5c131bc80e5d
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2023-03-27 00:17:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.07 ms
+메모리: 33.3 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,16 +1,10 @@
 function solution(s) {
-    const info = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    let cur = ''
-    let result = s.split("").map((v) => {
-        if (!isNaN(v)) return v
-        else {
-            cur += v
-            if (info.includes(cur)) {
-                let idx = info.indexOf(cur)
-                cur = ''
-                return idx
-            }
-        }
-    })
-    return +result.join("")
+    const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    
+    let result = s;
+    
+    for (let i = 0; i < num.length; i++) {
+        result = result.split(num[i]).join(i);
+    }
+    return Number(result);
 }

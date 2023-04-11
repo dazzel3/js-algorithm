34625072476321cf60fe95bda1a92083d296f892
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2023-04-12 01:42:28 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.69 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,10 +1,11 @@
 function solution(s) {
-    let count = 0, zero = 0
+    let count = 0, zero = 0;
+    
     while (s !== '1') {
-        let arr = s.split("").filter((v) => v === '1')
-        zero += (s.length - arr.length)
-        s = arr.length.toString(2)
-        count++
+        const arr = s.split("").filter((v) => v === '1');
+        zero += (s.length - arr.length);
+        s = arr.length.toString(2);
+        count++;
     }
-    return [count, zero]
+    return [count, zero];
 }
